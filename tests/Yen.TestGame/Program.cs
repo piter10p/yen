c5d6bfbe82aeb5e2119452aa16b7e1d5a5ed1dee
Piotr Paszko <piotr.paszko.dev@gmail.com>
2021-08-02 21:17:44 +0200

    Create solution

---
+++ tests/Yen.TestGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Yen.TestGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Engine())
+                game.Run();
+        }
+    }
+}
